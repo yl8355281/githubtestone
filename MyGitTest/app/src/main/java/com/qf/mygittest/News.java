851ba866e42b91a03379cdb6a2e89f48dccb4894
yl8355281 <574014652@qq.com>
2016-08-29 21:09:31 +0800

    Signed-off-by: yl8355281 <574014652@qq.com>

--- MyGitTest/app/src/main/java/com/qf/mygittest/News.java
+++ MyGitTest/app/src/main/java/com/qf/mygittest/News.java
@@ -4,4 +4,22 @@ package com.qf.mygittest;
  * Created by Administrator on 2016/8/29 0029.
  */
 public class News {
+    private String name;
+    private String pwd;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
 }
